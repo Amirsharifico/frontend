bd510ef5d8136800c3b47b622768b26ee0429970
Amir <amirsharifico@gmail.com>
2022-09-19 23:43:11 -0400

    11

--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -54,10 +54,10 @@ function SigninScreen(props) {
           <button type="submit" className="button primary">Signin</button>
         </li>
         <li>
-          New to amazona?
+          New to BOOBAADY?
         </li>
         <li>
-          <Link to={redirect === "/" ? "register" : "register?redirect=" + redirect} className="button secondary text-center" >Create your amazona account</Link>
+          <Link to={redirect === "/" ? "register" : "register?redirect=" + redirect} className="button secondary text-center" >Create your  account</Link>
         </li>
       </ul>
     </form>
